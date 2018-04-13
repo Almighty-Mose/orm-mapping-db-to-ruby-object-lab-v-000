7382296d9325c6f8a6455bd3713c392dd09675c9
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-13 16:45:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.first_x_students_in_grade_10(i)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
       FROM students
@@ -86,6 +86,6 @@ class Student
       LIMIT ?
       SQL
       
-    DB[:conn].execute(sql, i)
+    DB[:conn].execute(sql, x)
   end
 end
