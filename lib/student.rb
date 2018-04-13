9a069c0269e71913b6ca51dcf3da7d69e8578c96
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-13 16:50:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,9 +94,8 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT 1
       SQL
       
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
   end
 end
