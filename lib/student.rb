197a64ba1af535065c2decfa184d1ce5ffde0993
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-13 16:39:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,12 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
+      LIMIT 1
+      SQL
+    
+    DB[:conn].execute(sql, name).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
